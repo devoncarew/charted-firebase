dac374916d9ce72a25e107978f7d028f2846ddd4
Devon Carew <devoncarew@google.com>
2016-05-01 09:38:53 -0700

    initial commit

---
+++ tool/sdks.dart
@@ -0,0 +1,155 @@
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math' as math;
+
+import 'package:firebase/firebase_io.dart';
+
+FirebaseClient firebase;
+
+// TODO: take an arg for the sdk to use
+
+main(List<String> args) async {
+  if (args.length != 1) {
+    print('usage: dart tool/sdks <sdk version>');
+    exit(1);
+  }
+
+  if (Platform.environment['FIREBASE_CRED'] == null) {
+    print('Please set FIREBASE_CRED.');
+    exit(1);
+  }
+
+  List<SdkRelease> releases = [
+    // new SdkRelease('1.15.0-dev.0.0', new DateTime(2016, DateTime.JANUARY, 28)),
+    // new SdkRelease('1.15.0-dev.1.0', new DateTime(2016, DateTime.FEBRUARY, 2)),
+    new SdkRelease('1.15.0-dev.2.0', new DateTime(2016, DateTime.FEBRUARY, 10)),
+    new SdkRelease('1.15.0-dev.3.0', new DateTime(2016, DateTime.FEBRUARY, 17)),
+    new SdkRelease('1.15.0-dev.4.0', new DateTime(2016, DateTime.FEBRUARY, 24)),
+    new SdkRelease('1.15.0-dev.5.0', new DateTime(2016, DateTime.MARCH, 2)),
+    new SdkRelease('1.15.0-dev.5.1', new DateTime(2016, DateTime.MARCH, 9)),
+
+    new SdkRelease('1.16.0-dev.0.0', new DateTime(2016, DateTime.MARCH, 10)),
+    new SdkRelease('1.16.0-dev.1.0', new DateTime(2016, DateTime.MARCH, 16)),
+    new SdkRelease('1.16.0-dev.2.0', new DateTime(2016, DateTime.MARCH, 22)),
+    new SdkRelease('1.16.0-dev.3.0', new DateTime(2016, DateTime.MARCH, 30)),
+    new SdkRelease('1.16.0-dev.4.0', new DateTime(2016, DateTime.APRIL, 6)),
+    new SdkRelease('1.16.0-dev.5.0', new DateTime(2016, DateTime.APRIL, 12)),
+    new SdkRelease('1.16.0-dev.5.1', new DateTime(2016, DateTime.APRIL, 17)),
+    new SdkRelease('1.16.0-dev.5.2', new DateTime(2016, DateTime.APRIL, 19)),
+    new SdkRelease('1.16.0-dev.5.3', new DateTime(2016, DateTime.APRIL, 19, 12)),
+    new SdkRelease('1.16.0-dev.5.4', new DateTime(2016, DateTime.APRIL, 20)),
+    new SdkRelease('1.16.0-dev.5.5', new DateTime(2016, DateTime.APRIL, 25)),
+
+    new SdkRelease('1.17.0-dev.0.0', new DateTime(2016, DateTime.APRIL, 26)),
+    new SdkRelease('1.17.0-dev.1.0', new DateTime(2016, DateTime.APRIL, 27)),
+  ];
+
+  firebase = new FirebaseClient(Platform.environment['FIREBASE_CRED']);
+
+  // TODO: use 'which'
+  Directory.current = new Directory('/Users/devoncarew/projects/flutter');
+
+  if (args.first == '--all') {
+    SdkRelease release = releases.singleWhere((release) => release.version == args.first);
+    await _measure(release);
+  } else {
+    for (SdkRelease release in releases.reversed) {
+      await _measure(release);
+    }
+  }
+}
+
+const int kRunCount = 3;
+
+Future _measure(SdkRelease release) async {
+  print('${release}');
+  File stamp = new File('bin/cache/dart-sdk.stamp');
+  if (stamp.existsSync()) stamp.deleteSync();
+  new File('bin/cache/dart-sdk.version').writeAsStringSync(release.version);
+  await _run('bin/cache/update_dart_sdk.sh', []);
+
+  // Run kRunCount times, take the fastest time.
+  num bestGalleryTime = 1000.0;
+
+  File benchmarkFile = new File('dev/benchmarks/mega_gallery/analysis_benchmark.json');
+
+  for (int i = 0; i < kRunCount; i++) {
+    if (benchmarkFile.existsSync()) benchmarkFile.deleteSync();
+    await _run('flutter', ['analyze', '--watch', '--benchmark'],
+      cwd: 'dev/benchmarks/mega_gallery', throwOnFail: false);
+    num time = _getBenchmarkTime(benchmarkFile);
+    print('gallery analysis in ${time} seconds.');
+    bestGalleryTime = math.min(bestGalleryTime, time);
+  }
+
+  print(await firebase.post(
+    Uri.parse('https://charted-firebase.firebaseio.com/analysis/gallery.json'), {
+      'date': release.date.millisecondsSinceEpoch,
+      'duration': bestGalleryTime,
+      'sdk': release.version
+    }
+  ));
+
+  // Run kRunCount times, take the fastest time.
+  num bestRepoTime = 1000.0;
+
+  benchmarkFile = new File('analysis_benchmark.json');
+
+  for (int i = 0; i < kRunCount; i++) {
+    if (benchmarkFile.existsSync()) benchmarkFile.deleteSync();
+    await _run('flutter', ['analyze', '--flutter-repo', '--benchmark'], throwOnFail: false);
+    num time = _getBenchmarkTime(benchmarkFile);
+    print('repo analysis in ${time} seconds.');
+    bestRepoTime = math.min(bestRepoTime, time);
+  }
+
+  print(await firebase.post(
+    Uri.parse('https://charted-firebase.firebaseio.com/analysis/repo.json'), {
+      'date': release.date.millisecondsSinceEpoch,
+      'duration': bestRepoTime,
+      'sdk': release.version
+    }
+  ));
+
+  print('');
+}
+
+num _getBenchmarkTime(File file) {
+  Map benchmark = JSON.decode(file.readAsStringSync());
+  var time = benchmark['time'];
+  return time is String ? double.parse(time) : time;
+}
+
+Future _run(String cmd, List<String> args, { String cwd, bool throwOnFail: true }) async {
+  print('$cmd ${args.join(' ')}');
+  Process process = await Process.start(cmd, args, workingDirectory: cwd);
+  process.stdout
+    .transform(UTF8.decoder)
+    .transform(const LineSplitter())
+    .listen(print);
+  process.stderr
+    .transform(UTF8.decoder)
+    .transform(const LineSplitter())
+    .listen(print);
+  int exitCode = await process.exitCode.timeout(new Duration(seconds: 200)).catchError((e) {
+    process.kill();
+    throw e;
+  });
+  if (throwOnFail) {
+    if (exitCode != 0) throw 'exit $exitCode';
+  }
+}
+
+class SdkRelease {
+  final String version;
+  final DateTime date;
+
+  SdkRelease(this.version, this.date);
+
+  String get dateString => '${date.year.toString().padLeft(4, '0')}-'
+    '${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
+
+  String toString() => '${version} (${dateString})';
+}
---
+++ web/main.dart
@@ -0,0 +1,150 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+// import 'dart:convert' show JSON;
+import 'dart:html' hide Event;
+
+import 'package:charted/charts/charts.dart';
+import 'package:firebase/firebase.dart';
+
+Firebase firebase;
+CartesianArea chartArea;
+
+Map<int, Measurement> repoMeasurements;
+Map<int, Measurement> galleryMeasurements;
+
+// TODO: Tooltips should include sdk and commit info.
+
+void main() {
+  updateTimeSeriesChart('#time-series-chart');
+
+  firebase = new Firebase("https://charted-firebase.firebaseio.com/");
+
+  Firebase repoAnalysis = firebase.child("analysis/repo");
+  Firebase galleryAnalysis = firebase.child("analysis/gallery");
+
+  DateTime startDate = new DateTime.now().subtract(new Duration(days: 90));
+
+  Query repoQuery = repoAnalysis
+    .orderByChild('date')
+    .startAt(key: 'date', value: startDate.millisecondsSinceEpoch)
+    .limitToLast(1000);
+  Query galleryQuery = galleryAnalysis
+    .orderByChild('date')
+    .startAt(key: 'date', value: startDate.millisecondsSinceEpoch)
+    .limitToLast(1000);
+
+  repoQuery.onValue.listen((Event event) {
+    repoMeasurements = {};
+    event.snapshot.forEach((DataSnapshot snapshot) {
+      Measurement measurement = new Measurement(snapshot.val());
+      repoMeasurements[measurement.dateMillis] = measurement;
+    });
+    _updateChart();
+  });
+  galleryQuery.onValue.listen((Event event) {
+    galleryMeasurements = {};
+    event.snapshot.forEach((DataSnapshot snapshot) {
+      Measurement measurement = new Measurement(snapshot.val());
+      galleryMeasurements[measurement.dateMillis] = measurement;
+    });
+    _updateChart();
+  });
+}
+
+void _updateChart() {
+  if (repoMeasurements == null || galleryMeasurements == null) return;
+
+  List<int> times = new List.from(new Set<int>()
+    ..addAll(repoMeasurements.keys)
+    ..addAll(galleryMeasurements.keys)
+  )..sort();
+
+  List data = times.map((int time) {
+    return [time, repoMeasurements[time]?.duration, galleryMeasurements[time]?.duration];
+  }).toList();
+
+  updateTimeSeriesChart('#time-series-chart', data);
+}
+
+class Measurement {
+  final Map map;
+
+  // {date: 1461740400000, duration: 18.599, sdk: 1.17.0-dev.1.0, commit: ab47234}
+  Measurement(this.map);
+
+  String get sdk => map['sdk'];
+  String get commit => map['commit'];
+  num get duration => map['duration'];
+  num get dateMillis => map['date'];
+
+  DateTime get date => new DateTime.fromMillisecondsSinceEpoch(dateMillis);
+
+  String get dateString {
+    DateTime d = date;
+    return '${d.year.toString().padLeft(4, '0')}-'
+      '${d.month.toString().padLeft(2, '0')}-'
+      '${d.day.toString().padLeft(2, '0')}';
+  }
+
+  String toString() => '${duration}s (${dateString})';
+}
+
+void updateTimeSeriesChart(String wrapperSelector, [List data]) {
+  if (chartArea != null) {
+    chartArea.data = new ChartData(_columnSpecs, data);
+    chartArea.draw();
+  } else {
+    DivElement wrapper = document.querySelector(wrapperSelector);
+    DivElement areaHost = wrapper.querySelector('.chart-host');
+    DivElement legendHost = wrapper.querySelector('.chart-legend-host');
+
+    data ??= _placeholderData;
+
+    ChartData chartData = new ChartData(_columnSpecs, data);
+    ChartSeries series = new ChartSeries("Flutter Analysis Times", [1, 2], new LineChartRenderer());
+    ChartConfig config = new ChartConfig([series], [0])..legend = new ChartLegend(legendHost);
+    ChartState state = new ChartState();
+
+    chartArea = new CartesianArea(
+      areaHost,
+      chartData,
+      config,
+      state: state
+    );
+
+    chartArea.addChartBehavior(new Hovercard(isMultiValue: true));
+    chartArea.addChartBehavior(new AxisLabelTooltip());
+
+    chartArea.draw();
+  }
+}
+
+String _printDurationVal(num val) {
+  if (val == null) return '';
+  return val.toStringAsFixed(1) + 's';
+}
+
+Iterable _columnSpecs = [
+
+  new ChartColumnSpec(
+    label: 'Time',
+    type: ChartColumnSpec.TYPE_TIMESTAMP
+    // formatter: (num val) => val.toString() + ' sdf'
+  ),
+  new ChartColumnSpec(
+    label: 'flutter analyze flutter-repo',
+    type: ChartColumnSpec.TYPE_NUMBER,
+    formatter: _printDurationVal
+  ),
+  new ChartColumnSpec(
+    label: 'analysis server mega_gallery',
+    type: ChartColumnSpec.TYPE_NUMBER,
+    formatter: _printDurationVal
+  )
+];
+
+Iterable _placeholderData = const [
+  const [1317452400000, 0.0, 0.0],
+  const [1317538800000, 0.0, 0.0],
+];
